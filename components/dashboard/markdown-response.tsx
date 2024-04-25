359016157a0905480ec3056ca23efbff5192b0aa
Tuan Pham <tuan.pnngoc@gmail.com>
2024-04-25 21:18:58 +0700

    update

--- components/dashboard/markdown-response.tsx
+++ components/dashboard/markdown-response.tsx
@@ -12,9 +12,9 @@ const MarkdownResponse: React.FC<MarkdownResponseProps> = ({ content }) => {
   return (
     <ReactMarkdown
       components={{
-        code({ node, inline, className, children, ...props }) {
+        code({ node, inlist, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
+          return !inlist && match ? (
             <SyntaxHighlighter
               {...props}
               style={atomDark}
